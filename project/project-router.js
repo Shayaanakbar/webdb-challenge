b7928ce08721f00c98812cedd5aae62b889756b4
shayaanakbar <shayaanakbar1106@gmail.com>
2019-06-01 11:43:02 -0500

    Added post/get request for project

--- project/project-router.js
+++ project/project-router.js
@@ -13,13 +13,26 @@ const knexConfig = {
 const db = knex(knexConfig);
 
 //check
-router.get('/', (req, res) => {
-  res.send('Hello World!')
+router.get('/', async (req, res) => {
+  try {
+    const projects = await db('projects');
+    res.status(200).json(projects);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post
-
-
+router.post('/', async (req, res) => {
+  try {
+    const [id] = await db('projects').insert(req.body);
+    const project = await db('projects')
+      .where({ id })
+      .first()
+      res.status(201).json(project)
+  } catch (error) {
+    res.status(500).json({ error: "there was an error posting that!" })
+  }
+});
 
 
 module.exports = router;
