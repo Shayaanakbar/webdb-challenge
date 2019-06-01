22f402925275d2c2eb67ebced9dca37e1c1b73a8
shayaanakbar <shayaanakbar1106@gmail.com>
2019-06-01 11:38:31 -0500

    Working on Project table

--- migrations/20190601111906_project_table.js
+++ migrations/20190601111906_project_table.js
@@ -1,8 +1,22 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = async function(knex) {
+  await knex.schema.createTable('actions', function(tbl) {
+
+    tbl.increments()
+
+    tbl
+      .string('name', 128)
+      .notNullable()
+
+    tbl
+      .string('description')
+      .notNullable()
+
+    tbl
+      .boolean('completed').defaultTo('false');
+  })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('projects')
 };
